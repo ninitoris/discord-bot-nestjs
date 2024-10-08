88f980fa98547e0dc369ac0e2a78d4b222ee82ef
ninitoris <stassir99@gmail.com>
2024-10-09 00:01:41 +0300

    test: add tg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
+import { TelegramBotTest } from './telegram';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('tg')
+  testTg(@Req() req: Request, @Res() res: Response) {
+    console.log('gol');
+    const telegram = new TelegramBotTest();
+    telegram.sendMsgInGroupChat();
+    return res.status(200);
   }
 }
